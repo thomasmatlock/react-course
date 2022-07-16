9c257cb06d73dd5d338737564422976abffb8e25
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-07-16 18:24:52 -0400

    125 done, with errors

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,130 +1,121 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../context/auth-context';
+
 const emailReducer = (state, action) => {
     // console.log(action.type, action);
-    if (action.type === 'USER_INPUT') { 
-        return {value: action.val, isValid:action.val.includes('@')}; 
+    if (action.type === 'USER_INPUT') {
+        return { value: action.val, isValid: action.val.includes('@') };
     }
-    if (action.type === 'USER_BLUR')    {
-        return {value: state.value, isValid: state.value.includes('@')}; // default return value. above is condition logic to set validity based on includes @
-     }
-    return {value: '', isValid: false}; // default return value. above is condition logic to set validity based on includes @
+    if (action.type === 'USER_BLUR') {
+        return { value: state.value, isValid: state.value.includes('@') }; // default return value. above is condition logic to set validity based on includes @
+    }
+    return { value: '', isValid: false }; // default return value. above is condition logic to set validity based on includes @
 };
 
 const passwordReducer = (state, action) => {
     // console.log(action.type, action);
     if (action.type === 'USER_INPUT') {
         return { value: action.val, isValid: action.val.trim().length > 6 };
-     
+
     }
-    if (action.type === 'USER_BLUR')    {
-        return {value: state.value, isValid: state.value.trim().length > 6}; // default return value. above is condition logic to set validity based on includes @
-     }
-    return {value: '', isValid: false}; // default return value. above is condition logic to set validity based on includes @
+    if (action.type === 'USER_BLUR') {
+        return { value: state.value, isValid: state.value.trim().length > 6 }; // default return value. above is condition logic to set validity based on includes @
+    }
+    return { value: '', isValid: false }; // default return value. above is condition logic to set validity based on includes @
 };
 // this function is located outside the Login function because it doesnt require any info or state from the login funciton. it doesnt need to interact with anything inside the Login component.
 // all the info this function needs will be passed into this funciton by React automatically
 const Login = (props) => {
-	// const [enteredEmail, setEnteredEmail] = useState('');
-	// const [emailIsValid, setEmailIsValid] = useState();
-	// const [enteredPassword, setEnteredPassword] = useState('');
-	// const [passwordIsValid, setPasswordIsValid] = useState();
-	const [formIsValid, setFormIsValid] = useState(false);
+    // const [enteredEmail, setEnteredEmail] = useState('');
+    // const [emailIsValid, setEmailIsValid] = useState();
+    // const [enteredPassword, setEnteredPassword] = useState('');
+    // const [passwordIsValid, setPasswordIsValid] = useState();
+    const [formIsValid, setFormIsValid] = useState(false);
 
-	const [emailState, dispatchEmail] = useReducer(emailReducer, {value:'', isValid: false});
-	const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value:'', isValid: false});
+    const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null });
+    const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
+    const authCtx = useContext(AuthContext);
 
-	// useEffect(() => {
-	// 	console.log('[Login] useEffect');
-	//  }, []);
-    const { isValid: emailIsValid} = emailState; // adding this alias basically allows the thing to not run, once its valid, even more characters, it wont run. this makes it so when these depencies change, it will run again, instead of so often
-    const { isValid: passwordIsValid} = passwordState;
-	useEffect(() => {
-		const identifier = setTimeout(() => { 
-			// console.log('checking form validity');
-			setFormIsValid(
-				emailIsValid && passwordIsValid // simple validation for email
-				);
-			}, 500);
-		return () => {
-			// console.log('CLEANUP'); // this runs first and clears the timeout, so the first function is cleared
-			clearTimeout(identifier);
-		}
-    // }, [setFormIsValid, emailState, passwordState]); // dependencies are easy, just add WHAT YOU ARE USING in the side effect function, aka setFormIsValid, enteredEmail and enteredPassword. make sure functions arent with the () characters
-    }, [ emailIsValid, passwordIsValid]); // dependencies are easy, just add WHAT YOU ARE USING in the side effect function, aka setFormIsValid, enteredEmail and enteredPassword. make sure functions arent with the () characters
+    // useEffect(() => {
+    // 	console.log('[Login] useEffect');
+    //  }, []);
+    const { isValid: emailIsValid } = emailState; // adding this alias basically allows the thing to not run, once its valid, even more characters, it wont run. this makes it so when these depencies change, it will run again, instead of so often
+    const { isValid: passwordIsValid } = passwordState;
+    useEffect(() => {
+        const identifier = setTimeout(() => {
+            // console.log('checking form validity');
+            setFormIsValid(
+                emailIsValid && passwordIsValid // simple validation for email
+            );
+        }, 500);
+        return () => {
+            // console.log('CLEANUP'); // this runs first and clears the timeout, so the first function is cleared
+            clearTimeout(identifier);
+        }
+        // }, [setFormIsValid, emailState, passwordState]); // dependencies are easy, just add WHAT YOU ARE USING in the side effect function, aka setFormIsValid, enteredEmail and enteredPassword. make sure functions arent with the () characters
+    }, [emailIsValid, passwordIsValid]); // dependencies are easy, just add WHAT YOU ARE USING in the side effect function, aka setFormIsValid, enteredEmail and enteredPassword. make sure functions arent with the () characters
 
     const emailChangeHandler = (event) => {
         // setEnteredEmail(event.target.value);
-        dispatchEmail({type: 'USER_INPUT', val: event.target.value});
+        dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
         // setFormIsValid(passwordState.isValid && event.target.value.includes('@'));
     };
-    
+
     const passwordChangeHandler = (event) => {
         // setEnteredPassword(event.target.value);
-        
-        dispatchPassword({type: 'USER_INPUT', val: event.target.value});
+
+        dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
         // setFormIsValid(enteredPassword.value.trim().length > 6 && enteredEmail.value.includes('@'));
         // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6 );
-        setFormIsValid(emailState.isValid && event.target.value.trim().length > 6 );
+        setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
     };
 
     const validateEmailHandler = () => {
         // setEmailIsValid(emailState.isValid);
-        dispatchEmail({type: 'USER_BLUR'});
+        dispatchEmail({ type: 'USER_BLUR' });
     };
 
     const validatePasswordHandler = () => {
         // setPasswordIsValid(enteredPassword.trim().length > 6);
-        dispatchPassword({type: 'USER_BLUR'});
+        dispatchPassword({ type: 'USER_BLUR' });
     };
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        authCtx.onLogin(emailState.value, passwordState.value);
     };
 
-    return ( <
-        Card className = { classes.login } >
-        <
-        form onSubmit = { submitHandler } >
-        <
-        div className = { `${classes.control} ${
-						emailState.isValid === false ? classes.invalid : ''
-					}` } >
-        <
-        label htmlFor = "email" > E - Mail < /label> <
-        input type = "email"
-        id = "email"
-        value = { emailState.value }
-        onChange = { emailChangeHandler }
-        onBlur = { validateEmailHandler }
-        /> < /
-        div > <
-        div className = { `${classes.control} ${
-						passwordState.isValid === false ? classes.invalid : ''
-					}` } >
-        <
-        label htmlFor = "password" > Password < /label> <
-        input type = "password"
-        id = "password"
-        value = { passwordState.value }
-        onChange = { passwordChangeHandler }
-        onBlur = { validatePasswordHandler }
-        /> < /
-        div > <
-        div className = { classes.actions } >
-        <
-        Button type = "submit"
-        className = { classes.btn }
-        disabled = {!formIsValid } >
-        Login <
-        /Button> < /
-        div > <
-        /form> < /
-        Card >
+    return (
+        <Card className={classes.login} >
+            <form onSubmit={submitHandler}>
+                <div className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''}`} >
+                    <label htmlFor="email" > E - Mail </label>
+                    <input type="email"
+                        id="email"
+                        value={emailState.value}
+                        onChange={emailChangeHandler}
+                        onBlur={validateEmailHandler} />
+                </div >
+                <div className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''}`} >
+                    <label htmlFor="password" > Password </label>
+                    <input type="password"
+                        id="password"
+                        value={passwordState.value}
+                        onChange={passwordChangeHandler}
+                        onBlur={validatePasswordHandler} />
+                </div >
+                <div className={classes.actions} >
+                    <Button type="submit"
+                        className={classes.btn}
+                        disabled={!formIsValid} >
+                        Login
+                    </Button> </div>
+            </form>
+        </Card >
     );
 };
 
--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // AuthContext is a obj that contains a component, not a component itself
 // React.createContext, which this creates app-wide global state
 // you need to do 2 things to use this, 1) provide it, and 2) consume it. MAN this is GREAT. consuming it, is basically listening to it
 // 1) providing it also means you need to "wrap" all the components that use it, inside of it, so they can listen to it
 // wrapping App in it, means everything in App will listen to it, and everything in App will be able to access it
 // need 
+// put default contexts into this
 const AuthContext = React.createContext(
     {
         isLoggedIn: false,
+        onLogout: () => { },
+        onLogin: (email, password) => { }
     }
 );
+
+export const AuthContextProvider = (props) => {
+    const { isLoggedIn, setIsLoggedIn } = useState(false);
+
+    useEffect(() => {
+        const storedUserLoginInfo = localStorage.getItem('isLoggedIn');
+        if (storedUserLoginInfo === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []); // arg1 is a function, it can be anonymous. arg2 is an array of dependencies
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+
+        setIsLoggedIn(false);
+    }
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1'); // this takes 2 args, key and value, for this 1 = logged in, 0 = not logged in
+
+        setIsLoggedIn(true);
+    }
+    return <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }}> {props.children}</AuthContext.Provider >
+
+}
 
 export default AuthContext;
