54fcfbb7f63cc0009d8919006d42f18b40e83c54
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-07-19 21:22:44 -0400

    140 done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
-import classes from './Cart.css';
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
 const Cart = props => {
     const cartItems = (
         <ul className={classes['cart-items']}>
@@ -6,16 +7,18 @@ const Cart = props => {
         </ul>
     );
 
-    return <div>
-        {cartItems}
-        <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>35.62</span>
-        </div>
-        <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
-            <button className={classes.button}></button>
-        </div>
-    </div>
+    return (
+        <Modal>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </Modal>
+    );
 }
 export default Cart;
