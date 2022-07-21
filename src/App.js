bf65a817755c8eb24a1f2be22db84294835207fe
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-07-21 15:35:14 -0400

    142 done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
-function App() {
+export function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
   const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
   const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
   return (
-    <Fragment>
+    <CartProvider>
 
       {cartIsShown && <Cart onClose={hideCartHandler} />}
 
@@ -21,8 +22,7 @@ function App() {
 
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
-
 export default App;
---
+++ src/store/CartProvider.js
@@ -0,0 +1,22 @@
+import CartContext from "./cart-context";
+
+const CartProvider = props => {
+
+    const addItemToCartHandler = item => { }
+    const removeItemFromCartHandler = id => { }
+
+    const CartContext = React.createContext({
+
+
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    });
+
+
+    return <CartContext.Provider value={CartContext}>
+        {props.children}
+    </CartContext.Provider>
+}
+export default CartProvider;
