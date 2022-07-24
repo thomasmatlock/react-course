7d350dab36c570b8006c151734931fd39427b9a9
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-07-24 18:48:36 -0400

    whoa

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -3,7 +3,7 @@ import classes from './MealsSummary.module.css';
 const MealsSummary = () => {
     return (
         <section className={classes.summary}>
-            <h2>Amazing Warp Food, Delivered To You</h2>
+            <h2>Truly Amazing Warp Food, Delivered To You</h2>
             <p>
                 Choose your favorite meal from our broad selection of available meals
                 and enjoy a delicious lunch or dinner at home.
